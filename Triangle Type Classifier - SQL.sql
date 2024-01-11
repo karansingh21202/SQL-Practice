b5e3801c7d999f18061f1a6207d774935c6443a5
karansingh21202 <131647871+karansingh21202@users.noreply.github.com>
2024-01-11 20:07:46 +0530

    Update and rename Triangle Type Classifier - SQL to Triangle Type Classifier - SQL.sql

---
+++ Triangle Type Classifier - SQL.sql
@@ -0,0 +1,13 @@
+# Triangle Type Classifier - SQL
+## SQL Query
+SELECT
+    CASE
+    WHEN A + B > C AND B + C > A AND A + C > B THEN
+    CASE
+    WHEN A = B AND B = C THEN 'Equilateral'
+    WHEN A = B OR B = C OR A = C THEN 'Isosceles'
+    WHEN A != B AND B != C AND A != C THEN 'Scalene'
+    END
+    ELSE 'Not A Triangle'
+    END 
+FROM TRIANGLES;
